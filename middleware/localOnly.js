b530738707bd767226c0d87649afd0a0ab71500c
Mark Vayngrib <mark@tradle.io>
2017-03-26 23:28:21 -0400

    fix private ip detection

--- middleware/localOnly.js
+++ middleware/localOnly.js
@@ -2,9 +2,19 @@
 const ip = require('ip')
 
 module.exports = function filterIps (req, res, next) {
-  if (ip.isPrivate(req.ip)) {
+  if (isPrivateRequest(req)) {
     next()
   } else {
     res.status(403).send('forbidden')
   }
+}
+
+function isPrivateRequest (req) {
+  const ips = [
+    req.ip,
+    req.get('x-forwarded-for'),
+    req.get('x-real-ip')
+  ].filter(val => val)
+
+  return ips.every(ipAddr => ip.isPrivate(ipAddr))
 }
